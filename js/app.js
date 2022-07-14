7a13db3dd9c158e537446a3e2969f15e58b37687
celeste angeleri <angelericeleste@gmail.com>
2022-07-14 20:02:17 -0300

    agrego texto de boton

---
+++ js/app.js
@@ -0,0 +1,11 @@
+const whatsapp = document.querySelector(".whatsapp");
+const msjeContacto = document.querySelector(".msjeContacto");
+
+whatsapp.onmouseover = function(){
+    msjeContacto.style.display = "flex";
+    msjeContacto.style.justifyContent = "center";
+};
+
+whatsapp.onmouseleave = function(e){ 
+    msjeContacto.style.display = "none";
+};
